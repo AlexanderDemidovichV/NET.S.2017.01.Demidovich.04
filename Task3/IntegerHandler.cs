9042bcc6c2021b0a12f3773c64ea188ee6c8963d
Alexander <alex.dem.v@gmail.com>
2017-03-20 10:22:13 +0300

    Add docs.

--- Task3/IntegerHandler.cs
+++ Task3/IntegerHandler.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Task3
 {
+    /// <summary>
+    /// Provides static methods for 
+    /// </summary>
     public static class IntegerHandler
     {
+        /// <summary>
+        /// Find next the bigger number by digit'str permutation.
+        /// </summary>
+        /// <param name="number">A positive integer that provides digits.</param>
+        /// <returns>The index of the first bigger value than <paramref name="number"/>, if found; otherwise, minus 1.</returns>
         public static int NextBiggerNumber(int number)
         {
             return NextNumber(number);
         }
 
+        /// <summary>
+        /// Find next the bigger number by digit'str permutation.
+        /// </summary>
+        /// <param name="number">A positive integer that provides digits.</param>
+        /// <returns>The index of the first bigger value than <paramref name="number"/>, if found; otherwise, minus 1.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException"><paramref name="number"/> is less than zero.</exception>
         private static int NextNumber(int number)
         {
+            if (number<=0)
+                throw new ArgumentOutOfRangeException();
+
             var list = new List<string>();
 
             foreach (var permutation in GetPermutations(number.ToString()))
@@ -27,13 +45,18 @@ namespace Task3
             return int.Parse(list[list.IndexOf(number.ToString()) + 1]);
         }
 
-        private static IEnumerable<string> GetPermutations(string s)
+        /// <summary>
+        /// Gets all possible combinations by chars permutations.
+        /// </summary>
+        /// <param name="str">A string provides chars.</param>
+        /// <returns>All possible permutation combinations.</returns>
+        private static IEnumerable<string> GetPermutations(string str)
         {
-            if (s.Length > 1)
-                return from ch in s
-                       from permutation in GetPermutations(s.Remove(s.IndexOf(ch), 1))
+            if (str.Length > 1)
+                return from ch in str
+                       from permutation in GetPermutations(str.Remove(str.IndexOf(ch), 1))
                        select string.Format($"{ch}{permutation}");
-            return new[] { s };
+            return new[] { str };
         }
     }
 }
